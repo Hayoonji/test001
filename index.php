58fe7760c40b44d4c45ff9878ecd7604dbe2f0f5
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2021-12-12 20:02:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
-<html>
-  <head>
-    <meta http-equiv="refresh" content="1; url=/index.html">
-  </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=  , initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+      <a href="main.html" class="w3-bar-item w3-button"><b>Yoonjistagram</b></a>
+      <!-- Float links to the right. Hide them on small screens -->
+      <div class="w3-right w3-hide-small">
+        <a href="aboutme.html" class="w3-bar-item w3-button">About me</a>
+        <a href="myfavorite.html" class="w3-bar-item w3-button">My favorite</a>
+        <a href="bookreview.html" class="w3-bar-item w3-button">Book review</a>
+        <a href="knowledgebase.html" class="w3-bar-item w3-button">Knowledge Base</a>
+        <a href="contactme.html" class="w3-bar-item w3-button">Contact me</a>
+      </div>
+    </div>
+</div>
+
+<!-- profile section-->
+<div id="profile-section">
+    <img id="profile-picture" src="yjh.png">
+    <div class="introduction">
+        <h2>h_y99nz</h2>
+        <h4>게시물 6 &nbsp&nbsp 팔로워 318 &nbsp&nbsp팔로잉 279</h4>
+        <p>하윤지</p>
+    </div>
+</div>
+<!-- Menu Section -->
+
+<hr>
+
+</body>
 </html>
